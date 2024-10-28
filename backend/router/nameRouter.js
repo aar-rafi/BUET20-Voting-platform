8374a9c42cad0f0f7dd5bf8878137bc70eaacd3a
aar-rafi <ssrabonislam2000@gmail.com>
2024-10-28 12:53:01 +0600

    ratelimiter-1

--- backend/router/nameRouter.js
+++ backend/router/nameRouter.js
@@ -3,6 +3,15 @@ const nameController = require('../controller/nameController');
 const {authenticate} = require('../controller/adminController');
 const nameRouter = express.Router();
 
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // max 100 requests per windowMs
+});
+
+// apply rate limiter to all(no) requests
+nameRouter.use(limiter);
 
 nameRouter.route('/')
 .get(authenticate,nameController.getAllNames);
